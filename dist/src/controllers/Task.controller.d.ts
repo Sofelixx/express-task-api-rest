b905c6fe92e54035f49691b530bb719e3c3a342d
sofelixx <s18paris@gmail.com>
2022-04-20 23:54:20 -0300

    se quito vercel

--- dist/src/controllers/Task.controller.d.ts
+++
@@ -1,5 +0,0 @@
-import type { Request, Response, NextFunction } from "express";
-export declare const TasksGetAll: (req: Request, res: Response, next: NextFunction) => Promise<void>;
-export declare const TaskCreate: (req: Request, res: Response, next: NextFunction) => Promise<void>;
-export declare const TaskUpdate: (req: Request, res: Response, next: NextFunction) => Promise<void>;
-export declare const TaskDelete: (req: Request, res: Response, next: NextFunction) => Promise<void>;
--- dist/src/routes/Task.routes.d.ts
+++
@@ -1,1 +0,0 @@
-export declare const TaskRoutes: import("express-serve-static-core").Router;
