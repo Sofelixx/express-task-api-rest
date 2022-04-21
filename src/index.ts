5007f08d169e198a0da00281524a7f2f0395f4e2
sofelixx <s18paris@gmail.com>
2022-04-21 10:42:47 -0300

    se cambio el puerto de 80 a 3000

--- src/index.ts
+++ src/index.ts
@@ -4,7 +4,7 @@ import { db } from "./database/database";
 async function main() {
   await db.sync({ alter: false });
   console.log("Connected to database");
-  app.listen(80, () => console.log("Server listening on port 80"));
+  app.listen(3000, () => console.log("Server listening on port 3000"));
 }
 
 main();
