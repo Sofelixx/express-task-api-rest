422e27c0ef0ee990f94e05da336269ace325b461
sofelixx <s18paris@gmail.com>
2022-04-22 00:40:18 -0300

    se cambio el usuario con el que se conecta a la bd

--- src/database/database.ts
+++ src/database/database.ts
@@ -1,6 +1,6 @@
 import { Sequelize } from "sequelize";
 
-export const db = new Sequelize("task", "s5c5gu4ei1nn", "pscale_pw_Ovw5vuVvcsrMx1AO3hyfvbzCaPsRUEnb33H8AAqeZhk", {
+export const db = new Sequelize("task", "hkh18rd73s0q", "pscale_pw_Z2Myg3H0h3DoAIzVylT0PeYqh2ar1nWlFVJg4tRbq4U", {
   host: "a6fat11636n9.aws-sa-east-1-1.psdb.cloud",
   dialect: "mysql",
   dialectOptions: {
