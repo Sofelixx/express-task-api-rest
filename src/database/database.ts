3e14dd34bde994aa7268278ccbdd0db26235ccae
Sofelix <35209294+Sofelixx@users.noreply.github.com>
2022-04-25 19:03:43 -0300

    se quito el dialecto

--- src/database/database.ts
+++ src/database/database.ts
@@ -2,10 +2,6 @@ import { Sequelize } from "sequelize";
 
 export const db = new Sequelize("task", process.env?.DB_USER || "user", process.env?.DB_PASSWD || "passwd", {
   host: process.env?.DB_HOST || "host",
-  dialect: "mariadb",
-   dialectOptions: {
-    ssl: {
-      rejectUnauthorized: false
-    }
+  dialect: "mariadb"
   }
 })
