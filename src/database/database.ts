8b7b35ce7654577375b6773bd30ba35c455e35a1
Sofelix <35209294+Sofelixx@users.noreply.github.com>
2022-04-25 18:52:09 -0300

    se agrego el dialecto devuelta

--- src/database/database.ts
+++ src/database/database.ts
@@ -2,5 +2,10 @@ import { Sequelize } from "sequelize";
 
 export const db = new Sequelize("task", process.env?.DB_USER || "user", process.env?.DB_PASSWD || "passwd", {
   host: process.env?.DB_HOST || "host",
-  dialect: "mariadb"
+  dialect: "mariadb",
+   dialectOptions: {
+    ssl: {
+      rejectUnauthorized: false
+    }
+  }
 })
